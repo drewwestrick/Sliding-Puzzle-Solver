261a48f5957f68b27886bc33446c8a36946fe7b2
Drew Westrick <drewwestrick@evcracing.com>
2014-02-26 11:52:37 -0500

    Initial Commit

---
+++ Machine Problem #1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Machine Problem #1
+//
+//  Created by Andrew Westrick on 2/26/14.
+//  Copyright (c) 2014 Andrew Westrick. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
